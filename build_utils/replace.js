7bb3ddf393339120df7d48714c28f84c3e0f99c5
Zhenya Mamenko <mamenko@gmail.com>
2024-10-12 17:31:25 +0100

    [FIX] Fixed build plugin

--- build_utils/replace.js
+++ build_utils/replace.js
@@ -1,12 +1,22 @@
+import MagicString from 'magic-string'
+
 export default function replace(options = {}) {
   const { from ,to } = options;
 
   return {
     name: 'replace',
 
-    renderChunk(code) {
-      if (!from) return null;
-      return code.replace(from, to);
+    renderChunk(code, chunk) {
+      if (!from || !code) return null;
+      const s = new MagicString(code);
+      return {
+        code: s.replaceAll(from, to).toString(),
+        map: s.hasChanged() ? s.generateMap({
+          source: chunk.fileName,
+          file: chunk.fileName + '.map',
+          includeContent: true,
+        }) : null,
+      };
     },
 
   };
